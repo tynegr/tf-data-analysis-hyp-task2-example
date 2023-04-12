fe453ac75e1de58a1519cd81b0428b1806b8ac11
tynegr <122130536+tynegr@users.noreply.github.com>
2023-04-12 23:43:46 +0300

    Update solution.py

--- solution.py
+++ solution.py
@@ -1,11 +1,12 @@
 import pandas as pd
 import numpy as np
+from scipy.stats import cramervonmises_2samp
 
-
-chat_id = 123456 # Ваш chat ID, не меняйте название переменной
+chat_id = 462449141 # Ваш chat ID, не меняйте название переменной
 
 def solution(x: np.array, y: np.array) -> bool:
     # Измените код этой функции
     # Это будет вашим решением
     # Не меняйте название функции и её аргументы
-    return ... # Ваш ответ, True или False
+    p_value = 0.08
+    return cramervonmises_2samp(x, y).pvalue < p_value# Ваш ответ, True или False
